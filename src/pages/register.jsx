f3eb570547fc246011f116ef74845c9fdc2ad90b
fkaaziebu <frederickaziebu1998@gmail.com>
2023-02-20 06:22:01 +0000

    Formik, Yup
    Worked on the error in the register.jsx file

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,65 +1,104 @@
-import React, {useState} from "react"
-import './register.css';
-import { useFormik } from 'formik';
-import * as Yup from "yup";
+import React, { useState } from "react";
+import "./register.css";
+import { Formik, useFormik } from "formik";
+import * as yup from "yup";
 
+const emailValidationSchema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("required"),
+});
 
-export const  Register = (props) => {
-  const[phone, setPhone]  = useState("");
-  const[email, setEmail]  = useState("");
+const initialValueEmail = {
+  email: "",
+};
 
+export const Register = (props) => {
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        
-            console.log(email)
-    };
- 
-     const formik = useFormik( {
-      initialValues:{
-           email:""
-           
-      },
-      validationSchema: Yup.object({
-       email: Yup.string().email("Invalid email address").required("email is a required field")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
 
-      }),
-      onSubmit:  () => {
-            formik.preventDefault()
-      // console.log(formik.values);
-      
-      } 
-     });
-console.log(formik.errors);
-     
-    
-    return (
-       
-         <div>
-         <h2 className ="head-text">Enter Email/Phone to recieve token</h2>
-          <form className="login-form"  >
-          <label htmlFor="email">Email</label>
-		<input id="email" 
-             name="email"
-             type="email"
-             placeholder="enter your email"
-             onChange={formik.handleChange} 
-             onBlur={formik.handleBlur}
+  //   const formik = useFormik({
+  //     initialValues: {
+  //       email: "",
+  //     },
+  //     validationSchema: yup.object({
+  //       email: yup
+  //         .string()
+  //         .email("Invalid email address")
+  //         .required("email is a required field"),
+  //     }),
+  //     onSubmit: () => {
+  //       formik.preventDefault();
+  //       // console.log(formik.values);
+  //     },
+  //   });
+  //   console.log(formik.errors);
 
-             value= {formik.values.email}
-             />
-             { formik.errors.email ? <p  className="errorStyle"  style= {{ color:"red" }} >{formik.errors.email}</p> : null}
+  const handleFormSubmit = (values, onSubmitProps) => {
+    /* LOGIC FOR SUBMITTING FORM */
+  };
 
-             <button className="send-btn"  type="submit" onClick={() => props.onFormSwitch("loginForm")}> Send </button>
-		  
-		   {/* <button  className="send-btn"  type="submit"  onChange={() =>  props.onFormSwitch("loginForm")}>Send</button> */}
+  return (
+    <div>
+      <h2 className="head-text">Enter Email/Phone to recieve token</h2>
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValueEmail}
+        validationSchema={emailValidationSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+          setFieldValue,
+          resetForm,
+        }) => (
+          <form className="login-form">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="enter your email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+              helperText={touched.lastName && errors.lastName}
+            />
+            {errors.email ? (
+              <p className="errorStyle" style={{ color: "red" }}>
+                {errors.email}
+              </p>
+            ) : null}
 
-		</form>
-		<button className="link-btn" type="button" onClick={() => props.onFormSwitch("Phone")}> Use phone  </button>
-    
-         </div>
-      
-        
-    )
-}
+            <button
+              className="send-btn"
+              type="submit"
+              onClick={() => props.onFormSwitch("loginForm")}
+            >
+              {" "}
+              Send{" "}
+            </button>
+
+            <button
+              className="link-btn"
+              type="button"
+              onClick={() => props.onFormSwitch("Phone")}
+            >
+              {" "}
+              Use phone{" "}
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 export default Register;
